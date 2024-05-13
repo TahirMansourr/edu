447696c625617a5130e96af33ee2353565255f0c
tahir-mansour <tahirelmag@gmail.com>
2024-05-14 00:07:30 +0300

    added a payment page but still not styled

---
+++ forms/paymentForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
+import React from "react";
+
+const appearance = {
+  theme: 'stripe',
+
+  variables: {
+    colorPrimary: '#0570de',
+    colorBackground: '#ffffff',
+    colorText: '#30313d',
+    colorDanger: '#df1b41',
+    fontFamily: 'Ideal Sans, system-ui, sans-serif',
+    spacingUnit: '2px',
+    borderRadius: '4px',
+    // See all possible variables below
+  }
+};
+
+export default function PaymentForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const cardElement = elements?.getElement("card");
+
+    try {
+      if (!stripe || !cardElement) return null;
+      const { data } = await axios.post("/api/create-payment-intent", {
+        data: { amount: 89 },
+      });
+      const clientSecret = data;
+
+      await stripe?.confirmCardPayment(clientSecret, {
+        payment_method: { card: cardElement },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <CardElement />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
